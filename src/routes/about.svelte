52e595336754235b0327ad022d46e0ae7acd1fec
Alyson Youngblood <alysonyoungblood@gmail.com>
2020-05-10 08:32:56 -0400

    Update wording

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -11,7 +11,7 @@
 <p>
   I design and build digital storytelling products. From data visualizations to
   interactives to animations, I explore the possibilities of digital
-  communication with interdisciplinary teams. I work in every stage of the
+  communication with interdisciplinary teams. I work at every stage of the
   creative process, including concept development, ideation, wireframing,
   prototyping, visual design, and front-end development.
 </p>
